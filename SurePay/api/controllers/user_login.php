8136bb546c6044162626ea1927b3639a3d97fee6
JoshBeals <alelebealsjoshua@gmail.com>
2019-07-11 09:31:59 +0100

    api beginning

---
+++ SurePay/api/controllers/user_login.php
@@ -0,0 +1,76 @@
+<?php
+
+    // required headers
+    header("Access-Control-Allow-Origin: http://localhost/surepay/api");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    // include json web token files
+    include_once '../config/core.php';
+    include_once '../vendors/php-jwt-master/src/BeforeValidException.php';
+    include_once '../vendors/php-jwt-master/src/ExpiredException.php';
+    include_once '../vendors/php-jwt-master/src/SignatureInvalidException.php';
+    include_once '../vendors/php-jwt-master/src/JWT.php';
+
+    use \Firebase\JWT\JWT;
+
+    // files needed to connect to database
+    include_once '../config/database.php';
+    include_once '../models/user.php';
+    
+    // get database connection
+    $database = new DB_Connect();
+    $db = $database->connect();
+    
+    // instantiate user object
+    $user = new User($db);
+
+    // get posted data
+    $data = json_decode(file_get_contents("php://input"));
+    
+    // set product property values
+    $user->email = $data->email;
+    $user->password = $data->password;
+    $email_exists = $user->emailExists();
+
+    // check if email exists and if password is correct
+    if($email_exists && password_verify($data->password, $user->password)){
+    
+        $token = array(
+        "iss" => $iss,
+        "aud" => $aud,
+        "iat" => $iat,
+        "nbf" => $nbf,
+        "data" => array(
+                "id" => $user->id,
+                "fullname" => $user->fullname,
+                "email" => $user->email
+            )
+        );
+    
+        // set response code
+        http_response_code(200);
+    
+        // generate jwt
+        $jwt = JWT::encode($token, $key);
+        echo json_encode(
+            array(
+                "status" => "1",
+                "message" => "Successful login.",
+                "jwt" => $jwt
+            )
+        );
+    
+    }else{
+
+        // set response code
+        http_response_code(401);
+    
+        // tell the user login failed
+        echo json_encode(array("status" => "0", "message" => "Login failed."));
+
+    }
+
+?>
